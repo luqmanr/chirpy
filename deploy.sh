c2d2d9c36602d9d96a9e39775e5f21e4b23f9426
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-12-06 10:55:39 +0700

    update log time, config, and posts

--- deploy.sh
+++ deploy.sh
@@ -14,10 +14,14 @@ docker run --rm -it \
         bundle; \
         bundle exec jekyll build --config _config_selfhosted.yml; \
         rm -r blog/; \
-        mv _site/ blog/"
+        mv _site/ blog/; \
+        mkdir _site; \
+        mv blog/ _site/"
 rsync -chavzP --delete-after blog/* ${HOST}:${DEPLOY_PATH}/${BASE_URL}
 
 echo "[${DATE}]" > build.log
-git add build.log _config.yml _config_selfhosted.yml _posts/ blog/
+git add build.log _config.yml _config_selfhosted.yml _posts/ _site/
 git commit -m "update log time, config, and posts"
 git push origin main
+
+git push origin -d gh-pages; git subtree push  --prefix _site/ origin gh-pages

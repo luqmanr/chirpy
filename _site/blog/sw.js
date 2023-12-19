73a7f394701db37e6b3ba0ebc1cf5ad3a7f71cfc
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-12-19 21:02:22 +0700

    update log time, config, and posts

--- _site/blog/sw.js
+++ _site/blog/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/blog/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1702993962';
+const cacheName = 'chirpy-1702994541';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {

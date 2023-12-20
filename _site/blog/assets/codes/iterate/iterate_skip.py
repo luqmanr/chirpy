25a42f70c0d0e3a9e973ed4588b4f524780b76c6
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-12-20 11:11:34 +0700

    update log time, config, and posts

---
+++ _site/blog/assets/codes/iterate/iterate_skip.py
@@ -0,0 +1,5 @@
+for number in range(5, 15):
+    if number % 2 == 0:
+        print(number)
+
+print("selesai!")
---
+++ _site/blog/assets/codes/iterate/test.py
@@ -0,0 +1,15 @@
+def check(x, y):
+    if (x * y) <= 1000:
+        print(x * y)
+    else:
+        print(x + y)
+
+# case 1
+a = 30
+b = 20
+check(a, b)
+
+# case 2
+a = 50
+b = 60
+check(a, b)

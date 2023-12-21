703799a394724c25aacbbe1f13ba39136fc10573
Luqman Rahardjo <luqman.rahardjo@gmail.com>
2023-12-21 13:31:24 +0700

    update log time, config, and posts

--- _site/blog/assets/js/data/swcache.js
+++ _site/blog/assets/js/data/swcache.js
@@ -19,12 +19,12 @@ const resource = [
   /* --- Favicons & compressed JS --- */
   
   
+    '/blog/assets/img/favicons/favicon.ico',
     '/blog/assets/img/favicons/android-chrome-192x192.png',
     '/blog/assets/img/favicons/android-chrome-512x512.png',
     '/blog/assets/img/favicons/apple-touch-icon.png',
     '/blog/assets/img/favicons/favicon-16x16.png',
     '/blog/assets/img/favicons/favicon-32x32.png',
-    '/blog/assets/img/favicons/favicon.ico',
     '/blog/assets/img/favicons/mstile-150x150.png',
     '/blog/assets/js/dist/categories.min.js',
     '/blog/assets/js/dist/commons.min.js',
